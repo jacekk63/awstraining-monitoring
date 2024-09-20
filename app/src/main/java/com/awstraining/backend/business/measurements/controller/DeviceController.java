b289240804728a374945fe79526636423ab97b30
Jacek Kulesz <jacek.kulesz@capgemini.com>
2024-09-20 13:20:56 +0200

    abc

--- app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
+++ app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
@@ -16,23 +16,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 @RequestMapping("device/v1")
 class DeviceController implements DeviceIdApi {
     private static final Logger LOGGER = LogManager.getLogger(DeviceController.class);
 
     private final MeasurementService service;
+    private final MeterRegistry meterRegistry;
 
     @Autowired
-    public DeviceController(final MeasurementService service) {
+    public DeviceController(final MeasurementService service,
+                            MeterRegistry meterRegistry) {
         this.service = service;
+        this.meterRegistry = meterRegistry;
     }
 
     @Override
     public ResponseEntity<Measurement> publishMeasurements(final String deviceId, final Measurement measurement) {
         LOGGER.info("Publishing measurement for device '{}'", deviceId);
+        publishMeasurements();
         final MeasurementDO measurementDO = fromMeasurement(deviceId, measurement);
+
         service.saveMeasurement(measurementDO);
+
         LOGGER.info("Retrieving all measurements for device id '{}'", measurementDO.getDeviceId());
         return ResponseEntity.ok(measurement);
     }
@@ -43,10 +52,40 @@ class DeviceController implements DeviceIdApi {
                 .stream()
                 .map(this::toMeasurement)
                 .toList();
+
+        retrieveMeasurements();
+
         final Measurements measurementsResult = new Measurements();
         measurementsResult.measurements(measurements);
         LOGGER.info("End-Retrieving all measurements for device '{}'", measurements.size());
         return ResponseEntity.ok(measurementsResult);
+    }
+
+    private void retrieveMeasurements() {
+        String methodName = new Object() {}
+            .getClass()
+            .getEnclosingMethod()
+            .getName();
+
+        Counter counter = Counter.builder("retrieveMeasurements.counter")
+            .tags("retrieveMeasurements", methodName)
+            .register(meterRegistry);
+
+        counter.increment();
+    }
+
+
+    private void publishMeasurements() {
+        String methodName = new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName();
+
+        Counter counter = Counter.builder("publishMeasurements.counter")
+                .tags("publishMeasurements", methodName)
+                .register(meterRegistry);
+
+        counter.increment();
     }
 
     private Measurement toMeasurement(final MeasurementDO measurementDO) {
